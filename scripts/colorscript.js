8b7ea160695c2d955938e22e7751b6249e1bd3e8
jakobutter <jakobutter@users.noreply.github.com>
2025-02-07 13:56:28 +0000

    deploy: jakobutter/jakobutterdotnet@acb89436d620b16bca8dd18f83ea2af9738573de

--- scripts/colorscript.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.colorButton').forEach(button => {
-    button.addEventListener('click', function() {
-        const newColor = this.getAttribute('data-color');
-        document.documentElement.style.setProperty('--color1', newColor);
-    });
-});
