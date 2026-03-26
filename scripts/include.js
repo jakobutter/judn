519b330ab321512f52ce19eac6aad9672a8b0258
jakobutter <jakobutter@users.noreply.github.com>
2026-03-26 03:28:46 +0000

    deploy: jakobutter/jakobutterdotnet@b0e0bfa5f819775159e48097901794cadf4aa63b

--- scripts/include.js
+++ scripts/include.js
@@ -3,6 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.text())
     .then(html => {
       document.getElementById("header").innerHTML = html;
+
+      
+      const d = new Date(document.lastModified);
+      const fmt = d.getFullYear().toString() +
+        (d.getMonth()+1).toString().padStart(2,'0') +
+        d.getDate().toString().padStart(2,'0');
+      document.getElementById("pagedate").innerHTML = fmt;
     })
     .catch(err => console.error("Header load failed:", err));
+
+  
+  document.querySelectorAll('body > div').forEach(div => {
+    const title = div.querySelector(':scope > a:first-child');
+    if (!title) return;
+    title.addEventListener('click', () => {
+      div.classList.toggle('collapsed');
+    });
+  });
 });
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
