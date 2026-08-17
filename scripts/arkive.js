f2777434e8dade0fff069e8edec11d988d645b56
jakobutter <smashkidx@gmail.com>
2026-08-16 18:17:21 -0700

    autocommit

--- scripts/arkive.js
+++ scripts/arkive.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }));
 
       document.body.insertAdjacentHTML("beforeend", `
-        <div>
+        <div style="--color1:${r.txt};--color2:${r.window};font-family:${r.font}">
           <a>${r.title}</a><br>
           <img id="pageArtwork" src="${coverUrl}"><br>
           <section id="track-list"></section><br>
