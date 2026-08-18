056d552b63646bc52008ba969af50a011f4ed42c
jakobutter <smashkidx@gmail.com>
2026-08-17 17:41:19 -0700

    autocommit

--- scripts/arkive.js
+++ scripts/arkive.js
@@ -3,11 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.text())
     .then(html => {
       document.getElementById("playa").innerHTML = html;
-    })
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
+    });
   fetch("/arkive.json")
     .then(res => res.json())
     .then(releases => {
